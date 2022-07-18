6548183be6b2dd329dec8c4ca6649efbd7af5c47
mpcrockett <mpcrockett+github@gmail.com>
2022-07-18 10:47:40 -0400

    introduced data module, functions to replace two arrays

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const quotes = require(quotes.js);
+const quoteArray = require('/Users/melissacrockett/HardDriveDocs/Projects/MixedMessages/database.js').quoteArray;
+
+const randomIndex = (array) => {
+   return Math.floor(Math.random() * array.length);
+};
+
+const characters = () => {
+    let array = [];
+    quoteArray.forEach(entry => { 
+        if (!array.includes(entry.character)) {
+        array.push(entry.character)
+      }
+    });
+    return array;
+  };
+
+const topics = () => {
+    let array = [];
+    quoteArray.forEach(entry => { 
+        if (!array.includes(entry.topic)) {
+        array.push(entry.topic)
+      }
+    });
+    return array;
+  };
 
 const generateRandomQuote = () => {
-	
-	const characters = ["Rachel", "Joey", "Chandler", "Phoebe", "Monica", "Ross"];
-    const topics = ["friendship", "love", "travel", "work", "wisdom"];
 
-	const randomIndex = (array) => {
-  	return Math.floor(Math.random() * array.length);
-  };
-  
-    const randomChar = char[randomIndex(char)];
+   const randomCharacter = characters()[randomIndex(characters())];
 
-    const randomTopic = topics[randomIndex(topics)];
+   const randomTopic = topics()[randomIndex(topics())];
   
-    const matchedQuotes = quotes.filter(object => object.character == randomChar && object.topic == randomTopic);  
+   const matchedQuotes = quoteArray.filter(object => object.character == randomCharacter && object.topic == randomTopic);  
   
-    const getRandomQuote = matchedQuotes[randomIndex(matchedQuotes)];
+   const getRandomQuote = matchedQuotes[randomIndex(matchedQuotes)];
   
-    console.log(`${getRandomQuote.quote} - ${getRandomQuote.character}, on the topic of ${getRandomQuote.topic}.`);
+   console.log(`${getRandomQuote.quote} - ${getRandomQuote.character}, on the topic of ${getRandomQuote.topic}.`);
 };
 
 generateRandomQuote();
