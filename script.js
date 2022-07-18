f6d64f6f3720ee79bbf42b9db168de57c96d7e26
mpcrockett <mpcrockett+github@gmail.com>
2022-07-18 14:24:30 -0400

    fix variables

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const quoteArray = require('/Users/melissacrockett/HardDriveDocs/Projects/MixedMessages/database.js').quoteArray;
-
+// helper functions
 const randomIndex = (array) => {
-   return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 };
 
 const valueArray = (x) => {
-   let array = [];
-   quoteArray.forEach(entry => { 
-     if (!array.includes(entry[x])) {
-       array.push(entry[x])
-      }
-    });
-    return array;
-    };
-
-const generateRandomQuote = () => {
-
-   const randomCharacter = valueArray("character")[randomIndex(valueArray("character"))];
-
-   const randomTopic = valueArray("topic")[randomIndex(valueArray("topic"))];
-  
-   const matchedQuotes = quoteArray.filter(object => object.character == randomCharacter && object.topic == randomTopic);  
-  
-   const getRandomQuote = matchedQuotes[randomIndex(matchedQuotes)];
-  
-   console.log(`${getRandomQuote.quote} - ${getRandomQuote.character}, on the topic of ${getRandomQuote.topic}.`);
+  let array = [];
+  quoteArray.forEach(entry => { 
+    if (!array.includes(entry[x])) {
+     array.push(entry[x])
+    }
+  });
+  return array;
 };
 
-generateRandomQuote();
+const characterArray = valueArray("character");
+
+const topicArray = valueArray("topic");
+
+const getRandomQuote = () => {
+  const randomCharacter = characterArray[randomIndex(characterArray)];
+  const randomTopic = topicArray[randomIndex(topicArray)]; 
+  const matchedQuotes = quoteArray.filter(object => object.character == randomCharacter && object.topic == randomTopic);  
+  const randomizedQuote = matchedQuotes[randomIndex(matchedQuotes)];
+  console.log(`${randomizedQuote.quote} - ${randomizedQuote.character}, on the topic of ${randomizedQuote.topic}.`);
+};
+
+getRandomQuote();
